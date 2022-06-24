13d1a3a8163ca634d20ee474480e7d081e3c0b2d
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-06-24 15:10:23 -0300

    feat(video, lesson & Event): implement routes and components

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -17,12 +18,12 @@ const Lesson = (props: LessonProps) => {
 
   return (
     <div>
-      <a href="#">
+      <Link to={`/event/lesson/${props.slug}`} className="group">
         <span className="text-gray-300">
           {availableDateFormatted}
         </span>
 
-        <div className="rounded border border-gray-500 p-4 mt-2">
+        <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
           <header className="flex items-center justify-between">
 
             {isLessonAvailable ? (
@@ -44,7 +45,7 @@ const Lesson = (props: LessonProps) => {
 
           <strong className="text-gray-200 mt-5 block">{props.title}</strong>
         </div>
-      </a>
+      </Link>
     </div>
   );
 };
